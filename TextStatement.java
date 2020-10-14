09f274b3c4e0efefe7307d7199e4fd6bfac0e687
gabrielsluz <gabrielsluz1999@gmail.com>
2020-10-14 16:57:48 -0300

    Commit 18

--- TextStatement.java
+++ TextStatement.java
@@ -23,18 +23,4 @@ public class TextStatement extends Statement {
       return result;
     }
 
-    public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = this.rentalRecord(aCustomer);
-
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for each rental
-         result += this.rentalFigures(each);
-      }
-      //add footer lines
-      result += this.rentalFooter(aCustomer);
-      return result;
-   }
-
 }
